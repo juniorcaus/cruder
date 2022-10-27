b32db9895ce413dbcaa16572ccacd817294f2d38
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2022-10-26 22:13:57 -0300

    estilizando a aplicação e criando botões

--- src/pages/Feed/feed.js
+++ src/pages/Feed/feed.js
@@ -1,12 +1,61 @@
 import React from 'react'
+import './feed.css';
 
 import { Link } from 'react-router-dom';
 import HeaderMain from '../../components/HeaderMain/HeaderMain';
+
+import More from '../../images/more.svg';
 
 function Feed() {
   return (
     <div>
       <HeaderMain />
+
+      <main>
+
+        <div className="cards">
+
+          <div className="card" >
+
+              <header>
+                <h2>Consumindo API</h2>
+                <img src={More} />
+              </header>
+
+              <div className="line" ></div>
+
+              <p>APRENDENDOC COMO CONSUMIR API COM REACTJS </p>
+
+              <div className="btns" >
+
+                <div className="btn-edit" >
+                  <Link to='/edit' >
+                  <button>Edit</button>
+                  </Link>
+                </div>
+
+                <div className="btn-readmore" >
+                  <Link to='/lermais' >
+                   <button>Ler mais</button>
+                  </Link>
+                </div>
+
+                <div className="btn-delete" >
+                  <button>Delete</button>
+                </div>
+
+
+
+              </div>
+
+
+
+          </div>
+
+        </div>
+
+      </main>
+
     </div>
   )
 }
--- src/pages/LerMais/lermais.js
+++ src/pages/LerMais/lermais.js
@@ -1,12 +1,38 @@
 import React from 'react'
+
 import Header from '../../components/Header/Header';
+import More from '../../images/more.svg';
 
 function LerMais() {
   return (
     <div>
     <Header />
 
-    <h1>PÁGINA LER MAIS</h1>
+    <div>
+      <main>
+
+        <div className="cards">
+
+          <div className="card" >
+
+              <header>
+                <h2>Consumindo API</h2>
+                <img src={More} />
+              </header>
+
+              <div className="line" ></div>
+
+              <p>APRENDENDOC COMO CONSUMIR API COM REACTJSPRENDENDOC COMO CONSUMIR API COM REACTJSPRENDENDOC COMO CONSUMIR API COM REACTJSPRENDENDOC COMO CONSUMIR API COM REACTJSPRENDENDOC COMO CONSUMIR API COM REACTJSPRENDENDOC COMO CONSUMIR API COM REACTJSPRENDENDOC COMO CONSUMIR API COM REACTJSPRENDENDOC COMO CONSUMIR API COM REACTJSPRENDENDOC COMO CONSUMIR API COM REACTJSPRENDENDOC COMO CONSUMIR API COM REACTJSPRENDENDOC COMO CONSUMIR API COM REACTJS </p>
+
+          </div>
+
+        </div>
+
+      </main>
+
+    </div>
+
+    
     </div>
   )
 }
