6283ec98b5cfb0521a254845f6956d0cfeabaf4a
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2022-10-28 23:02:44 -0300

    Validando Formulário com Yup

--- src/pages/Post/post.js
+++ src/pages/Post/post.js
@@ -5,10 +5,20 @@ import { Link } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 
 import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+const validationPost = yup.object().shape({
+    title:  yup.string().required("O titulo é obrigatório").max(10, "Máximo 10 caracteres") ,
+    description: yup.string().required("A descrição é obrigatória").max(100, "Máximo 100 caracteres"), 
+    content: yup.string().required("O conteudo é obrigatório").max(500, "Máximo 500 caracteres")
+  })
 
 function Post() {
 
-  const { register, handleSubmit, formState: {erros} } = useForm()
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(validationPost)
+  })
   const addPost = data => console.log(data);
 
   return (
@@ -28,16 +38,19 @@ function Post() {
               <div className="fields" >
                 <label>Título</label>
                 <input type="text"  name="title" {...register("title")} />
+                <p className="error-message">{errors.title?.message}</p>
               </div>
 
               <div className="fields" >
                 <label>Descrição</label>
                 <input type="text" name="description" {...register("description")} />
+                <p className="error-message">{errors.description?.message}</p>
               </div>
 
               <div className="fields" >
                 <label>Conteudo</label>
                 <textarea type="text" name="content" {...register("content")} ></textarea>
+                <p className="error-message">{errors.content?.message}</p>
               </div>
 
               <div className="btn-post" >
